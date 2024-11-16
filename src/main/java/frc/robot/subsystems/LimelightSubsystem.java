d833149c7a761c173effab8b2436b72a92875110
einstein2823 <automatons2823@gmail.com>
2024-11-16 10:28:03 -0600

    forgot to add this to last push

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -4,7 +4,6 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.networktables.NetworkTable;
@@ -13,25 +12,24 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.LimelightConstants;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 
 public class LimelightSubsystem extends SubsystemBase {
   DriveSubsystem m_driveSubsystem;
   NetworkTableEntry botpose_blue;
+  CommandJoystick m_driveJoystick;
 
   public final Field2d m_field = new Field2d();
   NetworkTable m_table;
-  public double tx = 0;
-  public double ty = 0;
-  public double tz = 0;
-  public double rx = 0;
-  public double ry = 0;
-  public double rz = 0;
-
-  PIDController turnPID = new PIDController(0.02, 0, 0);
+  public double tx;
+  public double ty;
+  public double tz;
+  public double rx;
+  public double ry;
+  public double rz;
+  public double tv;
+  public int m_targetAprilTagID;
 
   public LimelightSubsystem() {
     Shuffleboard.getTab("shuffleboard")
@@ -39,28 +37,22 @@ public class LimelightSubsystem extends SubsystemBase {
         .withWidget(BuiltInWidgets.kField);
     m_table = NetworkTableInstance.getDefault().getTable("limelight-delight");
     botpose_blue = m_table.getEntry("botpose_wpiblue");
+
   }
 
   @Override
   public void periodic() {
     double defaultValues[] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
     double[] botpose = botpose_blue.getDoubleArray(defaultValues);
-    double tx = m_table.getEntry("tx").getDouble(0);
-    double ty = m_table.getEntry("ty").getDouble(0);
+    tx = m_table.getEntry("tx").getDouble(0);
+    ty = m_table.getEntry("ty").getDouble(0);
+    tv = m_table.getEntry("tv").getDouble(0);
 
     Pose2d m_robotPose = new Pose2d(tx, ty, new Rotation2d());
 
     m_field.setRobotPose(m_robotPose);
-  }
 
-  public Command turnToAprilTagCommand(double tx) {
-    return new RunCommand(() -> {
-      double speed = turnPID.calculate(tx, 0);
-      m_driveSubsystem.m_leftMotor.set(speed);
-      m_driveSubsystem.m_rightMotor.set(-speed);
-    }).finallyDo(() -> {
-      m_driveSubsystem.m_leftMotor.set(0);
-      m_driveSubsystem.m_rightMotor.set(0);
-    });
+    // TODO: Test if default value works (ID 6 isn't on field but in april tags set)
+    m_targetAprilTagID = (int) m_table.getEntry("tid").getInteger(6);
   }
 }
