3fcfad8a0f1427f950c35dbf29024d0a725427d5
BigMac <teamautomatons2823@gmail.com>
2024-12-05 15:44:58 -0600

    drive forward and shoot auto

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -1,7 +1,9 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
@@ -15,5 +17,11 @@ public class Autos {
                 m_driveSubsystem.followPathCommand("LeftOneHoop Part2"),
                 new ShooterCommand(m_shooterSubsystem));
 
+    }
+    public static Command DriveForwardShoot(DriveSubsystem m_driveSubsystem, ShooterSubsystem m_shooterSubsystem) {
+        return new SequentialCommandGroup(
+            new InstantCommand(() -> m_driveSubsystem.driveStraight(), m_driveSubsystem),
+            new WaitCommand(3).withTimeout(2.0),
+            new ShooterCommand(m_shooterSubsystem));
     }
 }
